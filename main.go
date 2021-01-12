5d9402187b229cdf02cc6d0718d73ed75a1eb853
Liam Pulles <me@liampulles.com>
2021-01-12 21:57:20 +0200

    Basic version

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("so random")
+	pwd, err := os.Getwd()
+	failIfErr(err)
+	items, err := ioutil.ReadDir(pwd)
+	failIfErr(err)
+
+	rSource := rand.NewSource(time.Now().UnixNano())
+	rGen := rand.New(rSource)
+	idx := rGen.Int() % len(items)
+
+	picked := items[idx]
+	fmt.Println(picked.Name())
+}
+
+func failIfErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
